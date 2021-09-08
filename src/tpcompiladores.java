f3b81cab617f862f6fed391d88848848f59d7f50
Mequi <maalvarez@alumnos.exa.unicen.edu.ar>
2021-09-08 17:37:49 -0300

    asdasd

--- src/tpcompiladores.java
+++ src/tpcompiladores.java
@@ -2,15 +2,5 @@ public class tpcompiladores {
 
     public static void main(String[] args) {
 
-        //probando 123
-
-        //probando 456
-
-        // probando 789
-
-        // HOLA HOLA
-
-        // prueba 10
-
     }
 }
