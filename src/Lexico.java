40070b4155907b9dfcb8da77fa1f7d9ef07f736c
fran <gonzalezfranl10@gmail.com>
2021-09-16 20:51:54 -0300

    Matriz de transiciones y cositas de Token

--- src/Lexico.java
+++ src/Lexico.java
@@ -6,10 +6,23 @@ public class Lexico {
     public static int linea;
     public static int cursor;
 
+
+
+    //TOKENS SIN ASCII
+    public static final int IDE = 257;
+    public static final int CTE_UINT = 258;
+    public static final int MAYOR_IGUAL = 259;
+    public static final int MENOR_IGUAL = 260;
+    public static final int IGUAL_IGUAL = 261;
+    public static final int DISTINTO = 262;
+    public static final int CTE_DOUBLE = 263;
+    public static final int CADENA = 264;
+
+
     //PALABRAS RESERVADAS
-    public static final int IF = 0;
-    public static final int THEN = 1;
-    public static final int ENDIF = 2;
+    public static final int IF = 265;
+    public static final int THEN = 266;
+    public static final int ENDIF = 267;
     public static final int PRINT = 3;
     public static final int FUNC = 4;
     public static final int RETURN = 5;
@@ -27,10 +40,34 @@ public class Lexico {
 
     //ACCIONES SEMANTICAS
 
+
     //ERRORES
 
 
-    private int [][] matrizTransiciones;
+    private int [][] matrizTransiciones = {
+
+             // L   l   d   E   _   +   -   %   =   <   >   *   :   &   |   .  bl   tb \n  otros
+              { 1,  1,  2,  1,  1,  F,  F, 10, 15, 13, 14,  8, 16, 17, 18,  4,  0,  0,  0,  F}, //0
+              { 1,  1,  1,  1,  1,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //1
+              { F,  F,  2,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  3,  F,  F,  F,  F}, //2
+              { F,  F,  F,  F,  F,  5,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //3
+              {-1, -1,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, //4
+              {-1, -1, -1, -1, -1,  6,  6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, //5
+              {-1, -1,  7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, //6
+              { F,  F,  7,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //7
+              { F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  9,  F,  F,  F,  F,  F,  F,  F,  F}, //8
+              { 9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  0,  9}, //9
+              {10, 10, 10, 10, 10, 11, 10,  F, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 12, 10}, //10
+              {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, -1}, //11
+              {-1, -1, -1, -1, -1, 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}, //12
+              { F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //13
+              { F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //14
+              { F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F}, //15
+
+
+    };
+
+
 
     private AccionSemantica[][] matrizAcciones;
 
--- src/TablaSimbolos.java
+++ src/TablaSimbolos.java
@@ -1,3 +1,4 @@
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class TablaSimbolos {
@@ -7,4 +8,36 @@ public class TablaSimbolos {
     public TablaSimbolos() {
         simbolos = new Hashtable<>();
     }
+
+    public Integer getId(String s ){
+        if (simbolos.contains(s))
+            return simbolos.get(s);
+        else
+            return null;
+    }
+
+    public void agregarSimbolo (String lexema, Integer id){
+        if (simbolos.contains(lexema))
+            System.out.println("El simbolo" + lexema + "ya se encuentra en la  Tabla de Simbolos");
+        else
+            simbolos.put(lexema,id);
+    }
+
+    public boolean existeLexema(String s){
+        return simbolos.contains(s);
+    }
+
+    public void eliminarSimbolo(String lexema) {
+        simbolos.remove(lexema);
+    }
+
+    public void printTablaSimbolos() {
+        Enumeration iterador = simbolos.keys();
+        while (iterador.hasMoreElements()) {
+            String lexema = (String) iterador.nextElement();
+            System.out.println("Lexema: " + lexema + ", id: " + simbolos.get(lexema));
+        }
+    }
+
+
 }
